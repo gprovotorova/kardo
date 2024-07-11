fae643525ae1874faae51575ee3967b52af8fa39
Galina Karlova <provotorova.gala@yandex.ru>
2024-07-11 14:21:20 +0300

    kardo app

---
+++ src/main/java/com/kardoaward/kardo/KardoApplication.java
@@ -0,0 +1,13 @@
+package com.kardoaward.kardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KardoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kardoaward/kardo/KardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kardoaward.kardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
